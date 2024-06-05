31f213b42464ee3f1b1e0d9107148bc83137e4fd
sohelranasakib <sohelrany802@gmail.com>
2024-06-05 22:45:55 +0600

    demo

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import Container from './Container'
+import Flex from './Flex'
+import logo from "../assets/Logo.png"
+import { FaBars } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
+
+const Header = () => {
+    let [show, setShow] = useState(false)
+  return (
+    <section className=' h-[80px]'>
+    <Container>
+      <Flex className="justify-between items-center h-[80px]">
+      <div className=" w-[25%]">
+          <img src={logo} alt="" />
+      </div>
+      <div className=" w-[75%]">
+          <ul className={`lg:flex justify-center gap-x-10 lg:static text-center absolute duration-700 ease-out ${show == true ? "bg-[green] top-[100px] left-0 w-full" : "top-[100px] left-[-200px]"}`}>
+            <li className='font-sans font-bold text-[16px] lg:text-[#767676] text-white hover:text-[#262626]'>Home</li>
+            <li className='font-sans font-bold text-[16px] lg:text-[#767676] text-white hover:text-[#262626]'>Shop</li>
+            <li className='font-sans font-bold text-[16px] lg:text-[#767676] text-white hover:text-[#262626]'>About</li>
+            <li className='font-sans font-bold text-[16px] lg:text-[#767676] text-white hover:text-[#262626]'>Contacts</li>
+            <li className='font-sans font-bold text-[16px] lg:text-[#767676] text-white hover:text-[#262626]'>Journal</li>
+          </ul>
+      </div>
+     <div className=" lg:hidden" onClick={()=>setShow(!show)}>
+        {show == true ? <RxCross2 />:  <FaBars />}
+     
+     
+     </div>
+      </Flex>
+    </Container>
+    </section>
+  )
+}
+
+export default Header
